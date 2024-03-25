8179690dd4f004c4185bce088872fbff2c94bd41
Willian Simão <simaowillian2606@gmail.com>
2024-03-25 20:00:13 -0300

    Criação de objeto vazio(GameManager)

--- Scripts/Bird.cs
+++ Scripts/Bird.cs
@@ -6,7 +6,7 @@ public class Bird : MonoBehaviour
 {
     public Rigidbody2D rig;
     public float jumpForce;
-    public BirdStatus status = BirdStatus.Start;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +17,15 @@ public class Bird : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (status)
+        switch (GameManager.instance.status)
         {
-            case BirdStatus.Start:
+            case GameStatus.Start:
                 StartUpdate();
                 break;
-            case BirdStatus.Play:
+            case GameStatus.Play:
                 PlayUpdate();
                 break;
-            case BirdStatus.GameOver:
+            case GameStatus.GameOver:
                 GameOverUpdate();
                 break;
         }
@@ -36,7 +36,8 @@ public class Bird : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            status = BirdStatus.Play;
+            // status = BirdStatus.Play;
+            GameManager.instance.StartGame();
             rig.bodyType = RigidbodyType2D.Dynamic;
             Jump();
         }
@@ -64,12 +65,6 @@ public class Bird : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        status = BirdStatus.GameOver;
+        GameManager.instance.GameOver();
     }
-}
-
-public enum BirdStatus {
-    Start,
-    Play,
-    GameOver
 }
